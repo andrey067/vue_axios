358f9f6ef6afbdb82066ceb316728788d5384dea
Audrey  Ernesto de Lima <3970@ucdb.br>
2022-07-12 09:38:36 -0400

    Formatando template para mostrar dados no vue

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -6,13 +6,12 @@
                     <div class="bg-gray-800 rounded-md overflow-hidden">
                         <div class="border-b border-gray-700 px-4 py-3 flex items-center justify-between">
                             <div>Data</div>
-                            {{renderOutput}}
-                            <div id="status"></div>
+                            <div id="status" :class="statusElClass"></div>
                         </div>
 
                         <div class="p-4">
                             <div class="overflow-y-auto h-96">
-                                <pre><code id="data" class="language-js line-numbers"></code></pre>
+                                <pre class="text-left">{{ dados }}</pre>
                             </div>
                         </div>
                     </div>
@@ -25,7 +24,7 @@
 
                     <div class="p-4">
                         <div class="overflow-y-auto h-96">
-                            <pre><code id="headers" class="language-js line-numbers"></code></pre>
+                            <pre class="text-left">{{ headers }}</pre>
                         </div>
                     </div>
                 </div>
@@ -37,7 +36,7 @@
 
                     <div class="p-4">
                         <div class="overflow-y-auto h-96">
-                            <pre><code id="config" class="language-js line-numbers"></code></pre>
+                            <pre class="text-left">{{ config }}</pre>
                         </div>
                     </div>
                 </div>
@@ -48,47 +47,42 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-// import responseData from '@/models/ResponseData'
+import responseData from '@/models/ResponseData'
 
 export default defineComponent({
     name: "MainComponent",
     props: {
         request: {}
     },
+    data: () => ({
+        statusElClass: ''
+    }),
     methods: {
-        // renderOutput(response: responseData) {
-        // Status
-        //const status = response.status;
-        // statusEl.removeAttribute('class');
-        // let statusElClass = 'inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium';
-        // if (status >= 500) {
-        //     statusElClass += ' bg-red-100 text-red-800';
-        // } else if (status >= 400) {
-        //     statusElClass += ' bg-yellow-100 text-yellow-800';
-        // } else if (status >= 200) {
-        //     statusElClass += ' bg-green-100 text-green-800';
-        // }
-
-        // statusEl.innerHTML = status;
-        // statusEl.className = statusElClass;
-
-        // Data
-        // dataEl.innerHTML = JSON.stringify(response.data, null, 2);
-        // Prism.highlightElement(dataEl);
-
-        // Headers
-        // headersEl.innerHTML = JSON.stringify(response.headers, null, 2);
-        // Prism.highlightElement(headersEl);
-
-        // Config
-        // configEl.innerHTML = JSON.stringify(response.config, null, 2);
-        // Prism.highlightElement(configEl);
-        //},
+        fomateclass(status: number) {
+            this.statusElClass = 'inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium';
+            if (status >= 500) {
+                this.statusElClass += ' bg-red-100 text-red-800';
+            } else if (status >= 400) {
+                this.statusElClass += ' bg-yellow-100 text-yellow-800';
+            } else if (status >= 200) {
+                this.statusElClass += ' bg-green-100 text-green-800';
+            }
+        }
     },
     computed: {
-        renderOutput() {
-            console.log(this.request)
-            return '';
+        dados: function () {
+            let responsedataformatado = this.request as responseData;
+            this.fomateclass(responsedataformatado.status)
+
+            return JSON.stringify(responsedataformatado.data, null, 2);
+        },
+        headers: function () {
+            let responsedataformatado = this.request as responseData;
+            return JSON.stringify(responsedataformatado.headers, null, 2);
+        },
+        config: function () {
+            let responsedataformatado = this.request as responseData;
+            return JSON.stringify(responsedataformatado.config, null, 2);
         }
     }
 })
